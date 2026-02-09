5d69d55cb1e7282803acf3c13da93416194bcff1
Thomas0922 <126225935+Thomas0922@users.noreply.github.com>
2026-02-09 18:00:18 +0800

    Update common.h

--- include/bmctool/common.h
+++ include/bmctool/common.h
@@ -3,39 +3,47 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include <stdbool.h>
 
-/* 版本資訊 */
-#define BMCTOOL_VERSION_MAJOR 0
-#define BMCTOOL_VERSION_MINOR 1
-#define BMCTOOL_VERSION_PATCH 0
+// Error codes
+#define BMC_SUCCESS              0
+#define BMC_ERROR_INVALID_PARAM  -1
+#define BMC_ERROR_MEMORY         -2
+#define BMC_ERROR_NETWORK        -3
+#define BMC_ERROR_TIMEOUT        -4
+#define BMC_ERROR_PROTOCOL       -5
 
-/* 錯誤碼 */
+const char* bmc_error_str(int error_code);
+
+// Log levels
 typedef enum {
-    BMC_SUCCESS = 0,
-    BMC_ERROR_INVALID_PARAM = -1,
-    BMC_ERROR_MEMORY = -2,
-    BMC_ERROR_NETWORK = -3,
-    BMC_ERROR_TIMEOUT = -4,
-    BMC_ERROR_AUTH = -5,
-    BMC_ERROR_PROTOCOL = -6,
-    BMC_ERROR_UNKNOWN = -99
-} bmc_error_t;
-
-/* 日誌等級 */
-typedef enum {
-    LOG_LEVEL_DEBUG = 0,
-    LOG_LEVEL_INFO = 1,
-    LOG_LEVEL_WARN = 2,
-    LOG_LEVEL_ERROR = 3
+    LOG_LEVEL_ERROR = 0,
+    LOG_LEVEL_WARN  = 1,
+    LOG_LEVEL_INFO  = 2,
+    LOG_LEVEL_DEBUG = 3
 } log_level_t;
 
-/* 全域設定 */
 extern log_level_t g_log_level;
 
-/* 工具函式宣告 */
-const char* bmc_error_str(bmc_error_t err);
 void bmc_log(log_level_t level, const char* fmt, ...);
-void bmc_hexdump(const uint8_t* data, size_t len, const char* prefix);
+void bmc_hexdump(const uint8_t* data, size_t len);
 
-#endif /* BMCTOOL_COMMON_H */
+// Output format
+typedef enum {
+    OUTPUT_FORMAT_NORMAL,
+    OUTPUT_FORMAT_JSON,
+    OUTPUT_FORMAT_TABLE
+} output_format_t;
+
+extern output_format_t g_output_format;
+
+// Table functions
+void table_init(int num_cols, const char* headers[]);
+void table_print_header(void);
+void table_print_row(const char* cols[]);
+void table_print_footer(void);
+
+// Simple formatting
+void print_kv(const char* key, const char* value);
+void print_section_header(const char* title);
+
+#endif
